778d266fe6c60e72f2af150963d4e09b4c795b86
unknown <viola4lfe@gmail.com>
2020-05-19 17:53:00 -0400

    continuing work

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,7 +3,12 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
+    for i in arrA:
+        for j in arrB:
+            if arrA[i] > arrB[j]:
+                merged_arr.append(arrB[j])
+            else:
+                merged_arr.append(arrA[i])
 
 
     return merged_arr
@@ -12,21 +17,57 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
 
+        merge_sort(left)
+        merge_sort(right)
+
+        # arr = merge(left, right) 
+        
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+              # The value from the left half has been used
+              arr[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k]=right[j]
+            j += 1
+            k += 1
 
     return arr
 
-
-# implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
 
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+
 
 
     return arr
